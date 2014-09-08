1ed5f95bc92c78f7929d0d70b7b2e0d1ae50c57e
Kristijan Burnik <kristijanburnik@gmail.com>
2014-09-09 00:52:57 +0200

    Basic color theme change

---
+++ chromiumcs_content_script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The background page is asking us to find an address on the page.
+if (window == top) {
+  chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
+    // sendResponse(findAddress());
+  });
+}
+
+// Search the text nodes for a US-style mailing address.
+// Return null if none is found.
+
