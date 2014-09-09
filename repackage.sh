483dc34c1ed3d2458a6dfd19d158b5b7247135a4
Kristijan Burnik <kristijanburnik@gmail.com>
2014-09-09 22:56:57 +0200

    Use lightweight repackaging tags

--- repackage.sh
+++ repackage.sh
@@ -3,7 +3,7 @@
 version=$(python reversion.py manifest.json)
 git add -u &&
   git commit --amend && \
-  git tag -s v"$version" -m "Release version: $version" && \
+  git tag v"$version" -m "Release version: $version" && \
   zip codesearch-theme.zip *.css *.json *.html *.js *.png
 echo "Release v. $version is ready"
 
