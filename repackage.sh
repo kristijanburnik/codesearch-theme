fab7ab117dbfff4f0c733949655c6843b36fcd75
Kristijan Burnik <kristijanburnik@gmail.com>
2014-09-09 22:59:49 +0200

    Repackaging simplified

--- repackage.sh
+++ repackage.sh
@@ -2,10 +2,10 @@
 
 version=$(python reversion.py manifest.json)
 git add -u &&
-  git commit --amend && \
+  git commit && \
   git tag v"$version" -m "Release version: $version" && \
+	git push origin master && \
   zip codesearch-theme.zip *.css *.json *.html *.js *.png
 echo "Release v. $version is ready"
-
 
 
