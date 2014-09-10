80e8a838266f151ef83868d98d2b5e4fa481aa32
Kristijan Burnik <kristijanburnik@gmail.com>
2014-09-11 00:53:19 +0200

    Multiple themes + theme roller + saving to sync storage

--- repackage.sh
+++ repackage.sh
@@ -5,7 +5,7 @@ git add -u &&
   git commit && \
   git tag v"$version" -m "Release version: $version" && \
 	git push origin master && \
-  zip codesearch-theme.zip *.css *.json *.html *.js *.png
+  zip codesearch-theme.zip css/*.css *.json *.html js/*.js *.png
 echo "Release v. $version is ready"
 
 
