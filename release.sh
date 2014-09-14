c9b6996362f6886ee8e2cd2d73edc821aef903fe
Kristijan Burnik <kristijanburnik@gmail.com>
2014-09-15 00:01:44 +0200

    Release fix: missing css/popup.css in zip file

--- release.sh
+++ release.sh
@@ -5,6 +5,6 @@ git add less/*.less js/*.js && git add -u &&
   git commit && \
   git tag v"$version" -m "Release version: $version" && \
 	git push origin master && \
-  zip codesearch-theme.zip css/generated.css *.json *.html js/*.js themes-icon.png
+  zip codesearch-theme.zip *.json *.html js/*.js css/generated.css css/popup.css themes-icon.png
 echo "Release v. $version is ready"
 exit 0
