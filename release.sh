690160ffcf051354613b03aafdc356ef3bc00d22
Kristijan Burnik <kristijanburnik@gmail.com>
2014-09-17 01:32:34 +0200

    New build system and refactoring

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-./rebuild.sh || exit 1
+./build.sh || exit 1
 version=$(python reversion.py manifest.json)
-git add less/*.less js/*.js && git add -u &&
+git add less js && git add -u &&
   git commit && \
   git tag v"$version" -m "Release version: $version" && \
 	git push origin master && \
-  zip codesearch-theme.zip *.json *.html js/*.js css/generated.css css/popup.css themes-icon.png
+  zip codesearch-theme.zip *.json *.html js/*.js css/*.css themes-icon.png
 echo "Release v. $version is ready"
 exit 0
